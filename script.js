35bc12e69e084c310bcd93c0bf224773ac9c8712
jacob hoefer <jacob.hoefer@gmail.com>
2023-09-20 20:21:26 -0400

    finished header time and local save data

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeBlocks = $('.time-block');
 $(function () {
-  var timeBlocks = $('.time-block');
   var currentHr = dayjs().hour();
+  setTime();
+  reset()
   timeBlocks.each(function (timeBlock) {
     timeBlock = $(this)
-    var hour = timeBlock.attr('id').split('-')[1];
+    var hour = timeBlock.attr('id').split('-') [1];
+  
     console.log(timeBlock, currentHr, hour);
     if(hour == currentHr) {
       timeBlock.addClass('present');
@@ -19,16 +21,51 @@ $(function () {
     else if (hour < currentHr) {
       timeBlock.addClass('past');
     }
-    })
+  })
   
-  });
-  // TODO: Add a listener for click events on the save button. This code should
-  var save = $('.time-block button');
+});
+setInterval(setTime, 60000);
+
+function reset() {
+  timeBlocks.removeClass('past');
+  timeBlocks.removeClass('future');
+  timeBlocks.removeClass('present');
+}
+
+
+// TODO: Add a listener for click events on the save button. This code should
+  var save = $('.saveBtn');
   // use the id in the containing time-block as a key to save the user input in
   function saveTodo() {
-    console.log ('save');
+    var btn = $(this);
+    var textArea = btn.prev().val();
+    var time = btn.parent().attr('id');
+    console.log(textArea);
+    localStorage.setItem(time, textArea);
   }
   save.click(saveTodo);
+
+  function setTime() {
+    currentDay.innerText = dayjs().format('ddd, MMMM D h:mm')
+  }
+
+  $('#hour-9 description').val(localStorage.getItem('hour-9'));
+
+  $('#hour-10 description').val(localStorage.getItem('hour-10'));
+
+  $('#hour-11 description').val(localStorage.getItem('hour-11'));
+
+  $('#hour-12 description').val(localStorage.getItem('hour-12'));
+
+  $('#hour-13 description').val(localStorage.getItem('hour-13'));
+
+  $('#hour-14 description').val(localStorage.getItem('hour-14'));
+
+  $('#hour-15 description').val(localStorage.getItem('hour-15'));
+
+  $('#hour-16 description').val(localStorage.getItem('hour-16'));
+  
+  $('#hour-17 description').val(localStorage.getItem('hour-17'));
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
